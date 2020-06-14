8a393bfbb07ead203b730b83cd9cd297b3bcb4d3
Gaurav Kumar <gs1465010@gmail.com>
2020-06-14 22:49:48 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -10,16 +10,15 @@ $message = '
  <div style="width:580px; height:415px; padding:20px; border: 4px solid #003399; text-align:center; color:#003399;">
      
        <br><br>
-       <span style="font-size:18px"><i>Total Shoutout<b> 58</b> 
+       <span style="font-size:18px"><i>Total Shoutout<b> 56</b> 
 </i></span><br>
-<span style="font-size:18px"><i>Total Shoutout This Week<b> 2</b> 
+<span style="font-size:18px"><i>Total Shoutout This Week<b> 3</b> 
 </i></span><br><br>
           <center>
       <span><img src="http://brahmarishisaubhari.com/feedback/Feedback/assets/img/logo.png"/></span> <br/><br/>
          </center>
 <table rules="all" style="border-color: #666;" cellpadding="10">
 <tr style="background: #eee;"><td><strong>Sender</strong> </td><td><strong>Receiver</strong> </td><td><strong>Quality Exhibited</strong> </td><td><strong>Date</strong> </td></tr>
-<tr><td><strong>Gaurav</strong> </td><td><strong>Roshan</strong> </td><td><strong>Collaboration</strong> </td><td><strong>Jan,23,2020 10:47:04 AM</strong> </td></tr>
 <tr><td><strong>Gaurav</strong> </td><td><strong>Roshan</strong> </td><td><strong>Collaboration</strong> </td><td><strong>Jan,23,2020 10:47:04 AM</strong> </td></tr>
 <tr><td><strong>Gaurav</strong> </td><td><strong>Roshan</strong> </td><td><strong>Collaboration</strong> </td><td><strong>Jan,23,2020 10:47:04 AM</strong> </td></tr>
 <tr><td><strong>Gaurav</strong> </td><td><strong>Roshan</strong> </td><td><strong>Collaboration</strong> </td><td><strong>Jan,23,2020 10:47:04 AM</strong> </td></tr>
