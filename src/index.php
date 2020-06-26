fb9e01bece2c10a098ac3d2b527d82f937632d7c
Gaurav Kumar <gs1465010@gmail.com>
2020-06-26 20:48:06 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 // Multiple recipients
-$to = 'nidhi.s@taboola.com, hadas.shmueli@taboola.com'; // note the comma
+$to = 'gaurav.k@taboola.com, gs1465010@gmail.com'; // note the comma
 
 // Subject
 $subject = 'Taboola Shoutout Weekly Report';
@@ -17,17 +17,12 @@ $message = '
        <br><br>
        <span style="font-size:18px;text-align: center;"><b style="color:#000;">Total shoutout sent since launch :<b style="color: #003399"> 57</b> 
 </i></span><br>
-<span style="font-size:18px;text-align: center;"><b style="color:#000;">Shoutout sent this week :<b style="color: #003399"> 1</b> 
+<span style="font-size:18px;text-align: center;"><b style="color:#000;">Shoutout sent this week :<b style="color: #003399"> 0</b> 
 </i></span><br><br>
           <center>
       <span><img src="http://brahmarishisaubhari.com/feedback/Feedback/assets/img/logo.png"/></span> <br/><br/>
          </center>
-<p style="color:#000;">Details of shoutout sent this week :</p>
-<table rules="all" style="border-color: #666;" cellpadding="10">
-<tr style="background: #eee; color:#000;"><td><strong>Sender</strong> </td><td><strong>Receiver</strong> </td><td><strong>Manager</strong> </td><td><strong>Quality Exhibited</strong> </td><td><strong>Date</strong> </td></tr>
-<tr><td>digvijay.s@taboola.com</td><td>navin.k@taboola.com</td><td>nuriel.a@taboola.com</td><td>Customer Centricity </td><td>Jun,15,2020 09:09:39 AM</td></tr>
-
-</table>       
+<p style="color:#000;">Details of shoutout sent this week : No shoutout sent this week</p>
 </div>';
 
 // To send HTML mail, the Content-type header must be set
@@ -36,7 +31,7 @@ $headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
 // Additional headers
 $headers[] = 'From: Shoutout Report<noreply>';
-$headers[] = 'Cc: jay.s@taboola.com, harel.u@taboola.com, sudip.c@taboola.com';
+$headers[] = 'Cc:';
 $headers[] = 'Bcc: gaurav.k@taboola.com';
 
 // Mail it
