45a8785021b2ae613637cc204790c84f73ad2a40
Gaurav Kumar <gs1465010@gmail.com>
2020-08-14 22:13:35 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,9 +15,9 @@ $message = '
 ">
      
        <br><br>
-       <span style="font-size:18px;text-align: center;"><b style="color:#000;">Total shoutout sent since launch :<b style="color: #003399"> 60</b> 
+       <span style="font-size:18px;text-align: center;"><b style="color:#000;">Total shoutout sent since launch :<b style="color: #003399"> 61</b> 
 </i></span><br>
-<span style="font-size:18px;text-align: center;"><b style="color:#000;">Shoutout sent this week :<b style="color: #003399"> 2</b> 
+<span style="font-size:18px;text-align: center;"><b style="color:#000;">Shoutout sent this week :<b style="color: #003399"> 1</b> 
 </i></span><br><br>
           <center>
       <span><img src="http://brahmarishisaubhari.com/feedback/Feedback/assets/img/logo.png"/></span> <br/><br/>
@@ -25,8 +25,7 @@ $message = '
 <p style="color:#000;">Details of shoutout sent this week :</p>
 <table rules="all" style="border-color:#666" cellpadding="10">
 <tbody><tr style="background:#eee;color:#000"><td><strong>Sender</strong> </td><td><strong>Receiver</strong> </td><td><strong>Manager</strong> </td><td><strong>Quality Exhibited</strong> </td><td><strong>Date</strong> </td></tr>
-<tr><td><a href="digvijay.s@taboola.com" target="_blank">digvijay.s@taboola.com</a></td><td><a href="katia.g@taboola.com" target="_blank">katia.g@taboola.com</a></td><td><a href="amit.w@taboola.com" target="_blank">amit.w@taboola.com</a></td><td>Collaboration </td><td>Aug,06,2020 11:52:31 AM</td></tr>
-<tr><td><a href="digvijay.s@taboola.com" target="_blank">digvijay.s@taboola.com</a></td><td><a href="benny.k@taboola.com" target="_blank">benny.k@taboola.com</a></td><td><a href="jay.s@taboola.com" target="_blank">jay.s@taboola.com</a></td><td>Empowering </td><td>Aug,06,2020 04:18:34 PM</td></tr>
+<tr><td><a href="anurag.k@taboola.com" target="_blank">anurag.k@taboola.com</a></td><td><a href="roshan.c@taboola.com" target="_blank">roshan.c@taboola.com</a></td><td><a href="sudip.c@taboola.com" target="_blank">sudip.c@taboola.com</a></td><td>Empowering</td><td>Aug,14,2020 09:35:50 AM</td></tr>
 </tbody></table>
 </div>';
 
